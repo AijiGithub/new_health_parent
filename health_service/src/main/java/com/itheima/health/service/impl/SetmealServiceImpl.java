b227de9951988e99ab977202e739973c0d44e794
lan <lanbixing@qq.com>
2020-11-08 00:31:19 +0800

    移动端页面使用redis缓存套餐数据

--- health_service/src/main/java/com/itheima/health/service/impl/SetmealServiceImpl.java
+++ health_service/src/main/java/com/itheima/health/service/impl/SetmealServiceImpl.java
@@ -3,6 +3,7 @@ package com.itheima.health.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.itheima.health.constant.JedisKeyConstant;
 import com.itheima.health.dao.SetmealDao;
 import com.itheima.health.entity.PageResult;
 import com.itheima.health.entity.QueryPageBean;
@@ -14,6 +15,9 @@ import com.itheima.health.service.SetmealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import sun.rmi.runtime.Log;
 
 import java.util.List;
 import java.util.Map;
@@ -34,6 +38,7 @@ public class SetmealServiceImpl implements SetmealService {
 
     /**
      * 添加套餐
+     *
      * @param setmeal
      * @param checkgroupIds
      */
@@ -45,10 +50,10 @@ public class SetmealServiceImpl implements SetmealService {
         // 获取套餐的id
         Integer setmealId = setmeal.getId();
         // 遍历循环选中的检查组id
-        if(null != checkgroupIds){
+        if (null != checkgroupIds) {
             for (Integer checkgroupId : checkgroupIds) {
                 // 添加套餐与检查组的关系
-                setmealDao.addSetmealCheckGroup(setmealId,checkgroupId);
+                setmealDao.addSetmealCheckGroup(setmealId, checkgroupId);
             }
         }
         return setmealId;
@@ -56,22 +61,24 @@ public class SetmealServiceImpl implements SetmealService {
 
     /**
      * 分页查询
+     *
      * @param queryPageBean
      * @return
      */
     @Override
     public PageResult<Setmeal> findPage(QueryPageBean queryPageBean) {
-        PageHelper.startPage(queryPageBean.getCurrentPage(),queryPageBean.getPageSize());
-        if(!StringUtils.isEmpty(queryPageBean.getQueryString())){
-            queryPageBean.setQueryString("%"+ queryPageBean.getQueryString()+"%");
+        PageHelper.startPage(queryPageBean.getCurrentPage(), queryPageBean.getPageSize());
+        if (!StringUtils.isEmpty(queryPageBean.getQueryString())) {
+            queryPageBean.setQueryString("%" + queryPageBean.getQueryString() + "%");
         }
         Page<Setmeal> page = setmealDao.findByCondition(queryPageBean.getQueryString());
-        PageResult<Setmeal> pageResult = new PageResult<Setmeal>(page.getTotal(),page.getResult());
+        PageResult<Setmeal> pageResult = new PageResult<Setmeal>(page.getTotal(), page.getResult());
         return pageResult;
     }
 
     /**
      * 通过id查询套餐信息
+     *
      * @param id
      * @return
      */
@@ -82,6 +89,7 @@ public class SetmealServiceImpl implements SetmealService {
 
     /**
      * 查询属于这个套餐的选中的检查组id
+     *
      * @param id
      * @return
      */
@@ -92,6 +100,7 @@ public class SetmealServiceImpl implements SetmealService {
 
     /**
      * 更新套餐
+     *
      * @param setmeal
      * @param checkgroupIds
      */
@@ -103,7 +112,7 @@ public class SetmealServiceImpl implements SetmealService {
         // 删除旧关系
         setmealDao.deleteSetmealCheckGroup(setmeal.getId());
         // 添加新关系
-        if(null != checkgroupIds){
+        if (null != checkgroupIds) {
             for (Integer checkgroupId : checkgroupIds) {
                 setmealDao.addSetmealCheckGroup(setmeal.getId(), checkgroupId);
             }
@@ -112,6 +121,7 @@ public class SetmealServiceImpl implements SetmealService {
 
     /**
      * 删除套餐
+     *
      * @param id
      */
     @Override
@@ -120,7 +130,7 @@ public class SetmealServiceImpl implements SetmealService {
         // 判断是否被订单使用
         int count = setmealDao.findOrderCountBySetmealId(id);
         // 使用则报错
-        if(count > 0){
+        if (count > 0) {
             throw new MyException("该套餐已经被订单使用了，不能删除");
         }
         // 没使用
@@ -132,6 +142,7 @@ public class SetmealServiceImpl implements SetmealService {
 
     /**
      * 获取套餐的所有图片
+     *
      * @return
      */
     @Override
@@ -141,6 +152,7 @@ public class SetmealServiceImpl implements SetmealService {
 
     /**
      * 套餐列表
+     *
      * @return
      */
     @Override
@@ -150,6 +162,7 @@ public class SetmealServiceImpl implements SetmealService {
 
     /**
      * 套餐详情
+     *
      * @param id
      * @return
      */
@@ -172,7 +185,7 @@ public class SetmealServiceImpl implements SetmealService {
         Setmeal setmeal = setmealDao.findById(id);
         // 查询套餐下的检查组
         List<CheckGroup> checkGroups = setmealDao.findCheckGroupListBySetmealId(id);
-        if(null != checkGroups){
+        if (null != checkGroups) {
             for (CheckGroup checkGroup : checkGroups) {
                 // 通过检查组id检查检查项列表
                 List<CheckItem> checkItems = setmealDao.findCheckItemByCheckGroupId(checkGroup.getId());
@@ -188,6 +201,7 @@ public class SetmealServiceImpl implements SetmealService {
 
     /**
      * 套餐预约占比
+     *
      * @return
      */
     @Override
